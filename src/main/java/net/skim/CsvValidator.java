3711d6935df28c9571f0bc17c74b1093de3bcc03
Sung Ho Kim <sungkim.1029@gmail.com>
2017-04-21 00:12:22 -0400

    Initial commit

---
+++ src/main/java/net/skim/CsvValidator.java
@@ -0,0 +1,10 @@
+package net.skim;
+
+/**
+ * Init
+ */
+public class CsvValidator {
+    public static void main(String[] args) {
+        System.out.println("CSV Validator");
+    }
+}
---
+++ src/test/java/net/skim/CsvValidatorTest.java
@@ -0,0 +1,33 @@
+package net.skim;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple CsvValidator.
+ */
+public class CsvValidatorTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CsvValidatorTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(CsvValidatorTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
